a06cc78e82c6fb22b981c6653a098825ad4f16e6
DaLLuZZ <khrushiov2012@yandex.ru>
2023-07-21 00:39:19 +0300

    STRIZH build target

---
+++ src/main/target/STRIZH/target.h
@@ -0,0 +1,113 @@
+/*
+ * This file is part of Cleanflight.
+ *
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define TARGET_BOARD_IDENTIFIER "STZH"
+
+#define USBD_PRODUCT_STRING  "STRIZH"
+
+#define LED0                    PD15
+#define LED1                    PD12
+#define LED2                    PD13
+
+#define BEEPER PD14 // LED (RED) + beeper
+#define BEEPER_INVERTED
+
+#define SENSORS_SET (SENSOR_ACC|SENSOR_GYRO)
+
+// *************** Gyro & ACC **********************
+#define USE_SPI
+
+#define USE_SPI_DEVICE_1
+#define SPI1_SCK_PIN            PA5
+#define SPI1_MISO_PIN           PA6
+#define SPI1_MOSI_PIN           PA7
+
+#define USE_IMU_BMI160
+#define IMU_BMI160_ALIGN        CW0_DEG
+#define BMI160_SPI_BUS          BUS_SPI1
+#define BMI160_CS_PIN			PA4
+
+// *************** Mag *****************************
+
+#define USE_I2C
+#define USE_I2C_DEVICE_1
+#define I2C1_SCL                PB6
+#define I2C1_SDA                PB7
+
+#define DEFAULT_I2C_BUS         BUS_I2C1
+
+#define USE_MAG
+#define MAG_I2C_BUS             BUS_I2C1
+#define USE_MAG_HMC5883
+
+
+// *************** UART ****************************
+#define USE_VCP
+#define VBUS_SENSING_PIN        PA9
+#define VBUS_SENSING_ENABLED
+
+#define USE_UART2
+#define UART2_RX_PIN            PA3
+#define UART2_TX_PIN            PA2
+
+#define USE_UART3
+#define UART3_RX_PIN            PC11
+#define UART3_TX_PIN            PC10
+
+#define USE_UART4
+#define UART4_RX_PIN            PA1
+#define UART4_TX_PIN            PA0
+
+#define USE_UART5
+#define UART5_RX_PIN            PD2
+#define UART5_TX_PIN            PC12
+
+#define USE_SOFTSERIAL1
+#define SOFTSERIAL_1_RX_PIN      PB1
+#define SOFTSERIAL_1_TX_PIN      PB0
+
+#define SERIAL_PORT_COUNT       6
+
+#define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
+#define SERIALRX_PROVIDER       SERIALRX_SBUS
+#define SERIALRX_UART           SERIAL_PORT_USART2
+
+// *************** ADC *****************************
+#define USE_ADC
+#define ADC_INSTANCE                ADC1
+#define ADC1_DMA_STREAM             DMA2_Stream0
+#define ADC_CHANNEL_1_PIN           PC5
+#define VBAT_ADC_CHANNEL            ADC_CHN_1
+
+#define DEFAULT_FEATURES (FEATURE_VBAT | FEATURE_TELEMETRY )
+
+#define USE_SPEKTRUM_BIND
+#define BIND_PIN PA2 // TX2
+
+#define USE_SERIAL_4WAY_BLHELI_INTERFACE
+
+#define TARGET_IO_PORTA         0xffff
+#define TARGET_IO_PORTB         0xffff
+#define TARGET_IO_PORTC         0xffff
+#define TARGET_IO_PORTD         0xffff
+
+#define USE_DSHOT
+#define USE_ESC_SENSOR
+
+#define MAX_PWM_OUTPUT_PORTS       6
